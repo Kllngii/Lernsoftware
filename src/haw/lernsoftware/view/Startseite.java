6562ab4ae55e6cc95a4176901ed732e2303fe915
MoritzKh <moritz.koch@haw-hamburg.de>
2023-04-21 10:50:41 +0200

    Kurztexte hinzugefügt und formatiert

--- src/haw/lernsoftware/view/Startseite.java
+++ src/haw/lernsoftware/view/Startseite.java
@@ -5,13 +5,19 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.Toolkit;
 
+import javax.swing.JEditorPane;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
+import javax.swing.text.html.HTMLEditorKit;
+
 import org.apache.log4j.Logger;
+
+import haw.lernsoftware.Konst;
+import haw.lernsoftware.resources.ResourceProvider;
 
 /**
  * Die Startseite des Programms.
@@ -58,7 +64,7 @@ public class Startseite extends HAWView {
 		c.gridy = 0; 
 		jp.add(ueberschrift, c);
 	    
-		JTextArea einleitungText = new JTextArea("Dieser Werkzeugkasten ebnet den Einstieg in das Thema „Freie Software“ in der Schule und stellt einige für die schulische Arbeit geeignete Werkzeuge vor, sodass Lehrkräfte in kurzer Zeit entscheiden können, ob und wie diese ihnen bei ihrer Arbeit helfen können. Die Auswahl erhebt natürlich keinen Anspruch auf Vollständigkeit, präsentiert jedoch eine Übersicht über Software zu allen schulisch relevanten Bereichen und porträtiert detailliert und praxisbezogen sieben wichtige Werkzeuge samt pädagogischer Einschätzung.");
+		JTextArea einleitungText = new JTextArea(ResourceProvider.loadStringFromProperties(Konst.PROPERTIES_STARTSEITE, "startseite.einleitungstext"));
 		einleitungText.setLineWrap(true);
 		einleitungText.setWrapStyleWord(true);
 		einleitungText.setEditable(false);
@@ -70,7 +76,10 @@ public class Startseite extends HAWView {
 		jp.add(einleitungText, c);
 		
 		
-		JTextArea functionText1 = new JTextArea("Tutorial");
+		JTextArea functionText1 = new JTextArea(ResourceProvider.loadStringFromProperties(Konst.PROPERTIES_STARTSEITE, "startseite.tutorial"));
+		functionText1.setLineWrap(true);
+		functionText1.setWrapStyleWord(true);
+		functionText1.setEditable(false);
 		//c.fill = GridBagConstraints.HORIZONTAL;
 		c.ipady = 0;      //make this component tall
 		c.weightx = 0.5;
@@ -79,7 +88,14 @@ public class Startseite extends HAWView {
 		c.gridy = 2;
 		jp.add(functionText1, c);
 		
-		JTextArea functionText2 = new JTextArea("Aufgaben");
+		JEditorPane functionText2 = new JEditorPane();
+		HTMLEditorKit kit = new HTMLEditorKit();
+		functionText2.setEditorKit(kit);
+		functionText2.setText(ResourceProvider.loadStringFromProperties(Konst.PROPERTIES_STARTSEITE, "startseite.aufgaben"));
+		//functionText2.setText("<html><body>In den <b>Aufgaben</b> zeigen wir dir, wie du deine Kenntnisse der Wahrscheinlichkeitstheorie auf die Probe stellen kannst, indem du Aufgaben mit verschiedenen Komplexitätsniveaus löst. Am Ende dieses Teils des Tutorials wirst du in der Lage sein, komplexe Aufgaben in der Wahrscheinlichkeitstheorie zu lösen und dein Wissen auf die nächste Stufe zu bringen.</body></html>");
+		//functionText2.setLineWrap(true);
+		//functionText2.setWrapStyleWord(true);
+		functionText2.setEditable(false);
 		//c.fill = GridBagConstraints.HORIZONTAL;
 		c.weightx = 0.5;
 		c.gridwidth = 1;
