69cc90a6c32af012a14d61b1bae086078626366e
MoritzKh <moritz.koch@haw-hamburg.de>
2023-04-28 09:23:20 +0200

    Texte in JLabel

--- src/haw/lernsoftware/view/Startseite.java
+++ src/haw/lernsoftware/view/Startseite.java
@@ -23,11 +23,11 @@ import haw.lernsoftware.resources.ResourceProvider;
  * Die Startseite des Programms.
  */
 public class Startseite extends HAWView {
-	
+
 	Logger log = Logger.getLogger(getClass());
 	JPanel jp;
 	private javax.swing.border.Border border = javax.swing.BorderFactory.createEtchedBorder(javax.swing.border.EtchedBorder.RAISED);
-	
+
 	public Startseite() {
 		constructStartseite();
 
@@ -36,37 +36,38 @@ public class Startseite extends HAWView {
 
 	private void constructStartseite() {
 		//panel.add(new JLabel("Das hier ist die Startseite"));
-		
-		
+
+
 		jp = new JPanel();
 		jp.setPreferredSize(new java.awt.Dimension(Toolkit.getDefaultToolkit().getScreenSize()));
 		//jp.setLayout(new FlowLayout(1, 100, 100)); // align from left to right, horizantal gap, vertical gap 
 		GridBagLayout gridbag = new GridBagLayout();
-	    jp.setLayout(gridbag);
-	    jp.setBorder(new EmptyBorder(0,300,0,300));			//Creates an empty border with the specified insets.
-	    GridBagConstraints c = new GridBagConstraints();
-	    c.anchor = GridBagConstraints.NORTH;
-	    c.fill = GridBagConstraints.HORIZONTAL;
-	    c.weighty = 1.0;
-	    c.insets = new Insets(5,10,0,10);
-	    
-	    HTMLEditorKit kit = new HTMLEditorKit();
-	    
-	    JLabel ueberschrift = new JLabel("Lernsoftware Startseite");
-	    ueberschrift.setHorizontalAlignment(SwingConstants.CENTER);
-	    ueberschrift.setFont(ueberschrift.getFont().deriveFont(40f));
-	    //c.fill = GridBagConstraints.CENTER;
-	    
+		jp.setLayout(gridbag);
+		jp.setBorder(new EmptyBorder(0,300,0,300));			//Creates an empty border with the specified insets.
+		GridBagConstraints c = new GridBagConstraints();
+		c.anchor = GridBagConstraints.NORTH;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weighty = 1.0;
+		c.insets = new Insets(5,10,0,10);
+
+		HTMLEditorKit kit = new HTMLEditorKit();
+
+		JLabel ueberschrift = new JLabel("Lernsoftware Startseite");
+		ueberschrift.setHorizontalAlignment(SwingConstants.CENTER);
+		ueberschrift.setFont(ueberschrift.getFont().deriveFont(40f));
+		//c.fill = GridBagConstraints.CENTER;
+
 		c.ipady = 40;      //make this component tall
 		c.weightx = 0.0;
 		c.gridwidth = 3;
 		c.gridx = 0;
 		c.gridy = 0; 
 		jp.add(ueberschrift, c);
-	    
+
 		//JEditorPane einleitungText = new JEditorPane();
 		JLabel einleitungText = new JLabel();
-		einleitungText.setBorder(this.border);
+		//einleitungText.TextAttribute.
+		//einleitungText.setBorder(this.border);
 		//einleitungText.setEditorKit(kit);
 		einleitungText.setText(ResourceProvider.loadStringFromProperties(Konst.PROPERTIES_STARTSEITE, "startseite.einleitungstext"));
 		//einleitungText.setLineWrap(true);
@@ -78,11 +79,11 @@ public class Startseite extends HAWView {
 		c.gridx = 0;
 		c.gridy = 1; 
 		jp.add(einleitungText, c);
-		
-		
+
+
 		//JEditorPane functionText1 = new JEditorPane();
 		JLabel functionText1 = new JLabel();
-		functionText1.setBorder(this.border);
+		//functionText1.setBorder(this.border);
 		//functionText1.setEditorKit(kit);
 		functionText1.setText(ResourceProvider.loadStringFromProperties(Konst.PROPERTIES_STARTSEITE, "startseite.tutorial"));
 		//functionText1.setLineWrap(true);
@@ -95,7 +96,7 @@ public class Startseite extends HAWView {
 		c.gridx = 0;
 		c.gridy = 2;
 		jp.add(functionText1, c);
-		
+
 		//JEditorPane functionText2 = new JEditorPane();
 		JLabel functionText2 = new JLabel();
 		//functionText2.setEditorKit(kit);
@@ -109,38 +110,39 @@ public class Startseite extends HAWView {
 		c.gridx = 1;
 		c.gridy = 2;
 		jp.add(functionText2, c);
-		
-		JEditorPane functionText3 = new JEditorPane();
-		functionText3.setEditorKit(kit);
+
+		//JEditorPane functionText3 = new JEditorPane();
+		JLabel functionText3 = new JLabel();
+		//functionText3.setEditorKit(kit);
 		functionText3.setText(ResourceProvider.loadStringFromProperties(Konst.PROPERTIES_STARTSEITE, "startseite.sandbox"));
 		c.weightx = 0.5;
 		c.gridwidth = 1;
 		c.gridx = 2;
 		c.gridy = 2;
 		jp.add(functionText3, c);
-		
-		
-	   
-        
-		
+
+
+
+
+
 		/*
 		jp.add(ueberschrift);
 		jp.add(einleitungText);
 		jp.add(functionText1);
 		jp.add(functionText2);
 		jp.add(functionText3);
-		
-*/
+
+		 */
 		panel.add(jp);
 	}
-	
+
 	//public JComponent buildContent() {
-		/*// Einfügen der Elemente
+	/*// Einfügen der Elemente
 		JLabel ueberschrift = new JLabel("Lernsoftware");
 		JTextArea einleitungText = new JTextArea("Dieser Werkzeugkasten ebnet den Einstieg in das Thema „Freie Software“ in der Schule und stellt einige für die schulische Arbeit geeignete Werkzeuge vor, sodass Lehrkräfte in kurzer Zeit entscheiden können, ob und wie diese ihnen bei ihrer Arbeit helfen können. Die Auswahl erhebt natürlich keinen Anspruch auf Vollständigkeit, präsentiert jedoch eine Übersicht über Software zu allen schulisch relevanten Bereichen und porträtiert detailliert und praxisbezogen sieben wichtige Werkzeuge samt pädagogischer Einschätzung.");
 		einleitungText.setLineWrap(true);
 		einleitungText.setWrapStyleWord(true);
-		
+
 		JLabel priceField = title("Title");
 		return FormBuilder.create()
 				//.columns("$label, $label, [100dlu, pref]")
@@ -151,9 +153,9 @@ public class Startseite extends HAWView {
 				.add(einleitungText).xy(1, 3)
 				.add(priceField) .xy(3, 5)
 				.build();*/
-		
-		
-		
-		
+
+
+
+
 	//}
 }
