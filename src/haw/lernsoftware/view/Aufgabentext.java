f37a144693cec9d6b71c13d47b0152ca940c5ff0
Push4ndPull <130736229+Push4ndPull@users.noreply.github.com>
2023-05-26 11:08:04 +0200

    Aufgabentext Antworten

--- src/haw/lernsoftware/view/Aufgabentext.java
+++ src/haw/lernsoftware/view/Aufgabentext.java
@@ -37,6 +37,7 @@ public class Aufgabentext extends HAWView implements ActionListener {
 	private JButton previousTaskButton = new JButton("PREVIOUS");
 	private JButton toLiniendiagrammButton = new JButton("Liniendiagramm");
 	private JTextArea aufgabenText = new JTextArea("if you can read this, report a bug");
+	private JTextArea loesungText = new JTextArea("if you can read this, report a bug");
 	private JComponent linienpanel;
 
 	private ImageIcon bild;
@@ -72,6 +73,13 @@ public class Aufgabentext extends HAWView implements ActionListener {
 		Color color = panel.getBackground();
 		aufgabenText.setBackground(color);
 
+		// Aufgabentext erstellen und formatieren
+		loesungText.setText("hier die Lösung!");
+		loesungText.setLineWrap(true);
+		loesungText.setPreferredSize(new Dimension(100, 100));
+		loesungText.setEditable(true);
+		// loesungText.setBackground(color);
+
 		// TODO was ist wenn auch noch Liniendiagramme dazukommen
 		if (model.getCurrentAufgabe().hasImage() == true) {
 			bild = new ImageIcon(model.getCurrentAufgabe().getImage());
@@ -88,17 +96,18 @@ public class Aufgabentext extends HAWView implements ActionListener {
 		// gibt einen JComponent zurück mit nur Text
 		return FormBuilder.create().debug(true) // Rote Linien zeichnen
 				.columns("100dlu, 5dlu, center:200dlu, 5dlu, 100dlu") //
-				.rows("p, 20dlu, p, $lg, top:100dlu, p") //
+				.rows("p, 20dlu, p, $lg, top:100dlu, p, p, top:50dlu") //
 				.padding(Paddings.DIALOG) //
 				.add(titleTaskLabel).xy(3, 1) //
+				.add(toLiniendiagrammButton).xy(5, 1) //
 				.add(previousTaskButton).xy(1, 2) //
 				.add(progress).xy(3, 2) //
 				.add(nextTaskButton).xy(5, 2) //
 				.addSeparator("Aufgabentext").xyw(1, 3, 5) //
-				.add(aufgabenText).xyw(1, 5, 3) //
-				.add(toLiniendiagrammButton).xy(5, 5) //
+				.add(aufgabenText).xyw(1, 5, 5) //
 				.add(aufgabenBild).xyw(1, 6, 5) // Muss noch als Funktion Variabel gemacht werden um Bilder zu laden
-				.add(linienpanel).xyw(1, 7, 5).build(); //
+				.add(linienpanel).xyw(1, 7, 5) //
+				.add(loesungText).xyw(1, 8, 5).build(); //
 	}
 
 	private void refreshAufgabenview() {
