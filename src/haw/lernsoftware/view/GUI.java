6071824053703199532eedb516d2b2707339b03f
Sebi888 <130735000+Sebi888@users.noreply.github.com>
2023-06-02 09:18:41 +0200

    Update GUI.java

--- src/haw/lernsoftware/view/GUI.java
+++ src/haw/lernsoftware/view/GUI.java
@@ -54,6 +54,7 @@ public class GUI implements ActionListener {
 	private Tutorial tutorialView = new Tutorial(this);
 	private Tutorial2 tutorial2View = new Tutorial2(this);
 	private Tutorial3 tutorial3View = new Tutorial3(this);
+
 	
 	public GUI(JFrame frame) {
 		this.frame = frame;
@@ -172,10 +173,13 @@ public class GUI implements ActionListener {
 			this.switchToView(WindowSelect.TUTORIAL);
 		} else if(e.getSource() == menuItemLeicht) {
 			log.info("Öffne das Leicht-Fenster!");
+			this.switchToView(WindowSelect.AUFGABENTEXT);
 		} else if(e.getSource() == menuItemMittel) {
 			log.info("Öffne das Mittel-Fenster!");
+			this.switchToView(WindowSelect.AUFGABENTEXT);
 		} else if(e.getSource() == menuItemSchwer) {
 			log.info("Öffne das Schwer-Fenster!");
+			this.switchToView(WindowSelect.AUFGABENTEXT);
 		}
 		
 	}
