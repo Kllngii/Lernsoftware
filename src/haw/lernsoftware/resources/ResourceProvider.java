1df8fb4a96f8e4abb0064509b4039353269ae05b
Push4ndPull <130736229+Push4ndPull@users.noreply.github.com>
2023-06-16 09:19:09 +0200

    neue aufgaben + log für bilder

--- src/haw/lernsoftware/resources/ResourceProvider.java
+++ src/haw/lernsoftware/resources/ResourceProvider.java
@@ -18,15 +18,20 @@ import java.util.Properties;
 import org.apache.log4j.Logger;
 
 import haw.lernsoftware.Konst;
+
 /**
- * Verwaltet (lesende) Dateizugriffe auf die Dateien im resources-Package. Hier sollen bspw. Aufgabendateien abgelegt werden.
+ * Verwaltet (lesende) Dateizugriffe auf die Dateien im resources-Package. Hier
+ * sollen bspw. Aufgabendateien abgelegt werden.
+ * 
  * @author Lasse Kelling
  */
 public class ResourceProvider {
 	private static Logger log = Logger.getLogger(ResourceProvider.class);
-	
+
 	/**
-	 * Liest eine Datei ein und gibt den Inhalt <b>aller Zeilen</b> zu einem String verbunden aus
+	 * Liest eine Datei ein und gibt den Inhalt <b>aller Zeilen</b> zu einem String
+	 * verbunden aus
+	 * 
 	 * @param path Der Pfad der Datei
 	 * @return Der Inhalt der Datei als String
 	 * 
@@ -34,41 +39,46 @@ public class ResourceProvider {
 	 */
 	public static String getFileContentAsString(String path) {
 		StringBuilder builder = new StringBuilder();
-		for(String row : getFileContent(path))
+		for (String row : getFileContent(path))
 			builder.append(row);
-		
+
 		return builder.toString();
 	}
+
 	/**
 	 * Liest eine Datei zu einer Liste von {@link String Zeilen} ein.
+	 * 
 	 * @param path Der Pfad der Datei
 	 * @return Der Inhalt der Datei als Liste von Strings
 	 */
 	public static List<String> getFileContent(String path) {
 		try {
-		Objects.requireNonNull(path);
-		URI pathURL = ResourceProvider.class.getResource(path).toURI();
-		Path p = Paths.get(pathURL);
-		log.debug("Einlesen der Datei " + path + " unter " + p.toAbsolutePath().toString());
-		
-		return Files.readAllLines(p);
-		} catch(NullPointerException e) { //Falls reqireNonNull fehlschlägt
+			Objects.requireNonNull(path);
+			URI pathURL = ResourceProvider.class.getResource(path).toURI();
+			Path p = Paths.get(pathURL);
+			log.debug("Einlesen der Datei " + path + " unter " + p.toAbsolutePath().toString());
+
+			return Files.readAllLines(p);
+		} catch (NullPointerException e) { // Falls reqireNonNull fehlschlägt
 			log.error(path);
 			log.error(e.getMessage());
-		} catch (URISyntaxException e) { //Falls die pathURL ungültig ist
+		} catch (URISyntaxException e) { // Falls die pathURL ungültig ist
 			log.error(path);
 			log.error(e.getMessage());
-		} catch (IOException e) { //Falls readAllLines fehlschlägt
+		} catch (IOException e) { // Falls readAllLines fehlschlägt
 			log.error(path);
 			log.error(e.getMessage());
 		}
-		
+
 		return new ArrayList<String>();
 	}
-	
+
 	/**
-	 * Lädt eine {@link Properties}-Datei und gibt den als <code>identifier</code> hinterlegten Wert der {@link Properties} zurück
-	 * @param props Die zu ladene {@link Properties}-Datei vermutlich in der Klasse {@link Konst} zu finden
+	 * Lädt eine {@link Properties}-Datei und gibt den als <code>identifier</code>
+	 * hinterlegten Wert der {@link Properties} zurück
+	 * 
+	 * @param props      Die zu ladene {@link Properties}-Datei vermutlich in der
+	 *                   Klasse {@link Konst} zu finden
 	 * @param identifier Der angeforderte Identifier
 	 * @return Der String der Identifiers
 	 * 
@@ -76,24 +86,32 @@ public class ResourceProvider {
 	 */
 	public static String loadStringFromProperties(String props, String identifier) {
 		Properties p = new Properties();
-		
-		try(FileInputStream fis = new FileInputStream(Paths.get(ResourceProvider.class.getResource(props).toURI()).toFile())) {
+
+		try (FileInputStream fis = new FileInputStream(
+				Paths.get(ResourceProvider.class.getResource(props).toURI()).toFile())) {
 			p.load(fis);
 		} catch (IOException e) {
 			log.error(e.getMessage());
 		} catch (URISyntaxException e) {
 			log.error(e.getMessage());
 		}
-		
+
 		return (String) p.getOrDefault(identifier, "ERROR");
 	}
+
 	/**
-	 * Lädt ein {@link Image} 
+	 * Lädt ein {@link Image}
+	 * 
 	 * @param path Der Pfad der Bilddatei relativ zum resources-Package
 	 * @return das Bild
 	 */
 	public static Image loadImage(String path) {
 		try {
+			Objects.requireNonNull(path);
+			URI pathURL = ResourceProvider.class.getResource(path).toURI();
+			Path p = Paths.get(pathURL);
+			log.debug("Einlesen der Datei " + path + " unter " + p.toAbsolutePath().toString());
+
 			return Toolkit.getDefaultToolkit().getImage(ResourceProvider.class.getResource(path).toURI().toURL());
 		} catch (MalformedURLException | URISyntaxException e) {
 			log.error(e.getMessage());
