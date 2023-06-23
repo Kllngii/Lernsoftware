e712c44c060ec8cd90a595b57c7debcf765c68c4
2308monsi <132046136+2308monsi@users.noreply.github.com>
2023-06-23 09:20:32 +0200

    Code-Kosmetik

--- src/haw/lernsoftware/Konst.java
+++ src/haw/lernsoftware/Konst.java
@@ -37,6 +37,5 @@ public class Konst {
 	public static final String PROPERTIES_AUFGABEN = "aufgaben.properties";
 	public static final int BORDER_X = 10;
 	public static final int BORDER_Y = 15;
-	public static final int STD_LINEWIDTH = 2;
 	public static final double DIGITS = 3.0;
 }
